0f966ce1ba1f20476fbaa959c9fe06f1272527ab
Samuel Cortés Sánchez <samuelcortessanchez@gmail.com>
2024-11-22 00:37:33 +0100

    Test del controlador de cuenta bancaria al 95

--- src/main/java/jyrs/dev/vivesbank/products/bankAccounts/dto/BankAccountResponse.java
+++ src/main/java/jyrs/dev/vivesbank/products/bankAccounts/dto/BankAccountResponse.java
@@ -1,5 +1,7 @@
 package jyrs.dev.vivesbank.products.bankAccounts.dto;
 
+import com.fasterxml.jackson.annotation.JsonCreator;
+import com.fasterxml.jackson.annotation.JsonProperty;
 import jakarta.annotation.Nullable;
 import jyrs.dev.vivesbank.products.bankAccounts.models.Type.AccountType;
 import jyrs.dev.vivesbank.products.creditCards.dto.CreditCardResponse;
@@ -14,4 +16,16 @@ public class BankAccountResponse {
     private double balance;
     @Nullable
     private CreditCardResponse creditCard;
+
+    @JsonCreator
+    public BankAccountResponse(
+            @JsonProperty("iban") String iban,
+            @JsonProperty("accountType") AccountType accountType,
+            @JsonProperty("balance") double balance,
+            @JsonProperty("creditCard") CreditCardResponse creditCard) {
+        this.iban = iban;
+        this.accountType = accountType;
+        this.balance = balance;
+        this.creditCard = creditCard;
+    }
 }
--- src/test/java/jyrs/dev/vivesbank/products/bankAccounts/controller/BankAccountControllerTest.java
+++ src/test/java/jyrs/dev/vivesbank/products/bankAccounts/controller/BankAccountControllerTest.java
@@ -3,13 +3,16 @@ package jyrs.dev.vivesbank.products.bankAccounts.controller;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import jyrs.dev.vivesbank.products.bankAccounts.dto.BankAccountRequest;
 import jyrs.dev.vivesbank.products.bankAccounts.dto.BankAccountResponse;
+import jyrs.dev.vivesbank.products.bankAccounts.exceptions.BankAccountNotFound;
 import jyrs.dev.vivesbank.products.bankAccounts.mappers.BankAccountMapper;
 import jyrs.dev.vivesbank.products.bankAccounts.models.BankAccount;
 import jyrs.dev.vivesbank.products.bankAccounts.models.Type.AccountType;
 import jyrs.dev.vivesbank.products.bankAccounts.services.BankAccountService;
 import jyrs.dev.vivesbank.products.creditCards.models.CreditCard;
 import jyrs.dev.vivesbank.utils.pagination.PageResponse;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.junit.jupiter.MockitoExtension;
@@ -23,11 +26,11 @@ import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort;
 import org.springframework.http.MediaType;
-import static org.mockito.Mockito.when;
-import static org.junit.jupiter.api.Assertions.assertAll;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.ArgumentMatchers.anyLong;
+import static org.mockito.Mockito.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
 import org.springframework.mock.web.MockHttpServletResponse;
 import org.springframework.test.web.servlet.MockMvc;
@@ -50,16 +53,10 @@ class BankAccountControllerTest {
     @MockBean
     private BankAccountMapper bankAccountMapper;
 
-    private final CreditCard card = new CreditCard();
-
-    private final BankAccount account = BankAccount.builder()
-            .id(1L)
-            .iban("ES91 2100 0418 4502 0005 1332")
-            .accountType(AccountType.STANDARD)
-            .balance(1.0)
-            .creditCard(card)
-            .build();
-
+    private BankAccount account;
+    private CreditCard card;
+    private BankAccountResponse bankAccountResponse;
+    private BankAccountRequest bankAccountRequest;
 
     @Autowired
     public BankAccountControllerTest(BankAccountService accountService, BankAccountMapper bankAccountMapper) {
@@ -68,9 +65,35 @@ class BankAccountControllerTest {
         mapper.registerModule(new JavaTimeModule());
     }
 
+    @BeforeEach
+    void setUp() {
+        mapper.registerModule(new JavaTimeModule());
+
+        card = new CreditCard();
+
+        account = BankAccount.builder()
+                .id(1L)
+                .iban("ES91 2100 0418 4502 0005 1332")
+                .accountType(AccountType.STANDARD)
+                .balance(1.0)
+                .creditCard(card)
+                .build();
+
+        bankAccountResponse = BankAccountResponse.builder()
+                .iban(account.getIban())
+                .accountType(account.getAccountType())
+                .balance(account.getBalance())
+                .creditCard(bankAccountMapper.toCardDto(account.getCreditCard()))
+                .build();
+
+        bankAccountRequest = BankAccountRequest.builder()
+                .accountType("STANDARD")
+                .build();
+    }
+
     @Test
     void findAll() throws Exception {
-        var accountList = List.of(bankAccountMapper.toResponse(account));
+        var accountList = List.of(bankAccountResponse);
         var pageable = PageRequest.of(0, 10, Sort.by("id").ascending());
         var page = new PageImpl<>(accountList);
 
@@ -83,7 +106,7 @@ class BankAccountControllerTest {
 
         PageResponse<BankAccount> res = mapper.readValue(response.getContentAsString(), new TypeReference<>() {});
 
-        assertAll("findall",
+        assertAll(
                 () -> assertEquals(200, response.getStatus()),
                 () -> assertEquals(1, res.content().size())
         );
@@ -91,5 +114,80 @@ class BankAccountControllerTest {
         verify(accountService, times(1)).findAllBankAccounts(Optional.empty(), pageable);
     }
 
+    @Test
+    void getById() throws Exception {
+        when(accountService.findBankAccountById(anyLong())).thenReturn(bankAccountResponse);
+        MockHttpServletResponse response = mockMvc.perform(
+                        get(myEndpoint + "/1")
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        BankAccountResponse res = mapper.readValue(response.getContentAsString(), BankAccountResponse.class);
+
+        assertAll(
+                () -> assertEquals(200, response.getStatus()),
+                () -> assertEquals(bankAccountResponse, res)
+        );
+
+        verify(accountService, times(1)).findBankAccountById(anyLong());
+    }
+
+    @Test
+    void getById_NotFound() throws Exception {
+        Long invalidId = 999L;
+        when(accountService.findBankAccountById(invalidId)).thenThrow(new BankAccountNotFound(invalidId));
+
+        MockHttpServletResponse response = mockMvc.perform(
+                        get(myEndpoint + "/" + invalidId)
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        assertEquals(404, response.getStatus());
+
+
+        verify(accountService, times(1)).findBankAccountById(invalidId);
+    }
+
+    @Test
+    void create() throws Exception {
+        when(accountService.saveBankAccount(any(BankAccountRequest.class))).thenReturn(bankAccountResponse);
+
+        MockHttpServletResponse response = mockMvc.perform(
+                        post(myEndpoint)
+                                .contentType(MediaType.APPLICATION_JSON)
+                                .content(mapper.writeValueAsString(bankAccountRequest))
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        assertAll(
+                () -> assertEquals(201, response.getStatus()),
+                () -> {
+                    BankAccountResponse res = mapper.readValue(response.getContentAsString(), BankAccountResponse.class);
+                    assertEquals(bankAccountResponse.getIban(), res.getIban());
+                    assertEquals(bankAccountResponse.getAccountType(), res.getAccountType());
+                    assertEquals(bankAccountResponse.getBalance(), res.getBalance());
+                    assertEquals(bankAccountResponse.getCreditCard(), res.getCreditCard());
+                }
+        );
+
+        verify(accountService, times(1)).saveBankAccount(any(BankAccountRequest.class));
+    }
+
+    @Test
+    void deleteById() throws Exception {
+        doNothing().when(accountService).deleteBankAccount(anyLong());
+
+        MockHttpServletResponse response = mockMvc.perform(
+                        delete(myEndpoint + "/1")
+                                .accept(MediaType.APPLICATION_JSON))
+                .andReturn().getResponse();
+
+        assertEquals(204, response.getStatus());
+
+        verify(accountService, times(1)).deleteBankAccount(1L);
+    }
+
 
 }
+
+
