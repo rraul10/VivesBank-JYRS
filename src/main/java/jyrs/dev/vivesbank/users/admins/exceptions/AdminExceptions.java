9e13f27ec2479c9f51a918a5726c86e3d78fa080
Javier Hernández vicente <javier.hernandez@alumno.iesluisvives.org>
2024-12-08 19:05:31 +0100

    Admin exceptions doc.

--- src/main/java/jyrs/dev/vivesbank/users/admins/exceptions/AdminExceptions.java
+++ src/main/java/jyrs/dev/vivesbank/users/admins/exceptions/AdminExceptions.java
@@ -3,24 +3,36 @@ package jyrs.dev.vivesbank.users.admins.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Excepciones del entorno de administración
+ */
 public abstract class AdminExceptions extends RuntimeException  {
 
         public AdminExceptions(String message) {
             super(message);
         }
-        @ResponseStatus(HttpStatus.NOT_FOUND)
+
+    /**
+     * Administrador no encontrado
+     */
+    @ResponseStatus(HttpStatus.NOT_FOUND)
         public static class AdminNotFound extends AdminExceptions{
             public AdminNotFound(String message) {
                 super(message);
             }
         }
 
+    /**
+     * Administrador ya existente, salta al intentar crear un admin que ya existe
+     */
     public static class AdminAlreadyExists extends AdminExceptions {
         public AdminAlreadyExists(String message) {
             super(message);
         }
     }
-
+    /**
+     * Administrador no puede ser eliminado.
+     */
     public static class AdminCannotBeDeleted extends AdminExceptions {
         public AdminCannotBeDeleted(String message) {
             super(message);
