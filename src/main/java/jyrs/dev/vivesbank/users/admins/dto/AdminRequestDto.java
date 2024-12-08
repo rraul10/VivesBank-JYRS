acdacd7e6919e990ee8f225c60e83c8c2287a14e
Javier Hernández vicente <javier.hernandez@alumno.iesluisvives.org>
2024-12-08 19:02:23 +0100

    Admin dtos doc.

--- src/main/java/jyrs/dev/vivesbank/users/admins/dto/AdminRequestDto.java
+++ src/main/java/jyrs/dev/vivesbank/users/admins/dto/AdminRequestDto.java
@@ -7,11 +7,18 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Dto para la request de la creación de un usuario
+ */
+
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AdminRequestDto {
+    /**
+     * Guuid perteneciente al usuario que pasará a ser administrador.
+     */
     @NotBlank(message = "El guuid no puede estar vacío")
     String guuid;
 
--- src/main/java/jyrs/dev/vivesbank/users/admins/dto/AdminUpdateRequest.java
+++ src/main/java/jyrs/dev/vivesbank/users/admins/dto/AdminUpdateRequest.java
@@ -7,16 +7,28 @@ import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
+/**
+ * Dto request para actualizar un administrador
+ */
 @Data
 @Builder
 @NoArgsConstructor
 @AllArgsConstructor
 public class AdminUpdateRequest {
+    /**
+     * Nombre de usuario
+     */
     @Email(regexp = ".*@.*\\..*", message = "User name debe ser válido")
     @NotBlank(message = "El nombre de usuario no puede estar vacío")
     String username;
+    /**
+     * Guuid del usuario
+     */
     @NotBlank(message = "El guuid no puede estar vacío")
     String guuid;
+    /**
+     * Ruta de la foto de perfil del usuario
+     */
     @NotBlank(message = "La ruta de la imagen no puede estar vacía")
     String fotoPerfil;
 
