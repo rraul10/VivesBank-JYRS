19ca00203862fec30b5684b75386e0cde7588f3f
Javier Hernández vicente <javier.hernandez@alumno.iesluisvives.org>
2024-12-08 18:11:22 +0100

    User excepctions doc.

--- src/main/java/jyrs/dev/vivesbank/users/users/exceptions/UserExceptions.java
+++ src/main/java/jyrs/dev/vivesbank/users/users/exceptions/UserExceptions.java
@@ -3,10 +3,18 @@ package jyrs.dev.vivesbank.users.users.exceptions;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
+/**
+ * Excepciones de usuarios
+ */
+
 public abstract class UserExceptions extends RuntimeException {
     public UserExceptions(String message) {
         super(message);
     }
+
+    /**
+     * Usuario no encontrado en el sistema
+     */
     @ResponseStatus(HttpStatus.NOT_FOUND)
     public static class UserNotFound extends UserExceptions {
         public UserNotFound(String message) {
