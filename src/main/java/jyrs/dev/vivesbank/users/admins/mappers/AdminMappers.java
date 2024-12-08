59c931886c563acbf867182dcc562288b65478c1
Javier Hernández vicente <javier.hernandez@alumno.iesluisvives.org>
2024-12-08 19:26:16 +0100

    Admin doc.

--- src/main/java/jyrs/dev/vivesbank/users/admins/mappers/AdminMappers.java
+++ src/main/java/jyrs/dev/vivesbank/users/admins/mappers/AdminMappers.java
@@ -5,12 +5,26 @@ import jyrs.dev.vivesbank.users.admins.dto.AdminResponseDto;
 import jyrs.dev.vivesbank.users.models.Admin;
 import org.springframework.stereotype.Component;
 
+/**
+ * Mapper para la request y response dtos de admin.
+ */
 @Component
 public class AdminMappers {
+    /**
+     * Mapea un adminRequestDto a un admin
+     * @param RequestDto
+     * @return admin
+     */
     public Admin fromAdminDto(AdminRequestDto RequestDto){
         return Admin.builder()
                 .guuid(RequestDto.getGuuid()).build();
     }
+
+    /**
+     * Mapea un admin a un adminResponseDto
+     * @param admin
+     * @return adminResponseDto
+     */
     public AdminResponseDto fromAdminToResponse(Admin admin){
         return AdminResponseDto.builder()
                 .guuid(admin.getGuuid())
--- src/main/java/jyrs/dev/vivesbank/users/admins/repository/AdminRepository.java
+++ src/main/java/jyrs/dev/vivesbank/users/admins/repository/AdminRepository.java
@@ -5,6 +5,14 @@ import jyrs.dev.vivesbank.users.models.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 
+/**
+ * Repositorio de administrador que extiende JpaRepository y JpaSpecificationExecutor para la paginación
+ */
 public interface AdminRepository extends JpaRepository<Admin, Long>, JpaSpecificationExecutor<Admin> {
+    /**
+     * Busca a un admin por su guuid
+     * @param guid
+     * @return
+     */
     Admin findByGuuid(String guid);
 }
