cf17eedc0293e1cac0e7124c6f2ccbc15abe9147
Samuceese <samuelcortessanchez@gmail.com>
2024-12-08 19:17:12 +0100

    Creando los perfiles para desarrollo y produccion

--- src/main/java/jyrs/dev/vivesbank/VivesBankApplication.java
+++ src/main/java/jyrs/dev/vivesbank/VivesBankApplication.java
@@ -1,6 +1,7 @@
 package jyrs.dev.vivesbank;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -13,12 +14,18 @@ import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 @Slf4j
 public class VivesBankApplication implements CommandLineRunner {
 
+    @Value("${spring.profiles.active}")
+    private String perfil;
+
+    @Value("${server.port}")
+    private String port;
+
     public static void main(String[] args) {
         SpringApplication.run(VivesBankApplication.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-        System.out.println("🟢 Servidor arrancado 🚀");
+        System.out.println("🟢 Servidor escuchando en puerto: " + port + " y perfil: " + perfil);
     }
 }
